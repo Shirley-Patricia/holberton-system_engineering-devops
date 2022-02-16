e4b55cabc16d910514a0271f70c01decaae659df
Shirley-Patricia <shircar16@gmail.com>
2022-02-15 22:08:53 -0500

    fixing pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """
-Write a function that queries the Reddit API and returns the number of 
+Write a function that queries the Reddit API and returns the number of
 subscribers (not active users, total subscribers) for a given subreddit.
 """
 
 import requests
+
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com//r/{}/about.json".format(subreddit)
