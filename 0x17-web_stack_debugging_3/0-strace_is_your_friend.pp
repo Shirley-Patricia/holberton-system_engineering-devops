98be5b099b54d1e00c07e5a09f07e2970260da53
Shirley-Patricia <shircar16@gmail.com>
2022-02-16 23:11:01 -0500

    fixing path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # using puppet to fix apache
 
-exec { 'stack_debug':
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
-  path    => '/usr/sbin/',
-  }
+exec {'stack_debug':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
